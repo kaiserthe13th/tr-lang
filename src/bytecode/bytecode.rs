130d24a6d3648d4d40a89f080274eb8e94c1469d
kaiserthe13th <superkerem13@gmail.com>
2021-10-21 23:37:07 +0300

    parser and bytecode done! v0.2

---
+++ src/bytecode/bytecode.rs
@@ -0,0 +1,11 @@
+use crate::token::{ ParserToken };
+use bincode;
+
+pub fn to_bytecode(parsed: Vec<ParserToken>) -> Vec<u8> {
+    bincode::serialize(&parsed).unwrap()
+}
+
+pub fn from_bytecode(bytecode: &[u8]) -> Vec<ParserToken> {
+    bincode::deserialize(bytecode).unwrap()
+}
+
