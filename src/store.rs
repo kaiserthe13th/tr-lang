0559032f5c6e7a81843fd7c0cf503db8f24dbfcf
kaiserthe13th <superkerem13@gmail.com>
2021-11-17 19:55:23 +0300

    change path separator depending on os

--- src/store.rs
+++ src/store.rs
@@ -1,2 +1,11 @@
+use cfg_if::cfg_if;
+
 pub const VERSION: &str = "0.3.1";
 pub const RELEASE: &str = "16.11.2021";
+cfg_if! {
+    if #[cfg(windows)] {
+        pub const PATH_SEP: char = '\\';
+    } else {
+        pub const PATH_SEP: char = '/';
+    }
+}
