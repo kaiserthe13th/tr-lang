1e788d60406680ccd133009d8fe219e0c24be9b6
kaiserthe13th <superkerem13@gmail.com>
2021-11-20 23:24:33 +0300

    add repr method to parsertokens

--- src/token/tokens.rs
+++ src/token/tokens.rs
@@ -121,4 +121,57 @@ impl ParserToken {
     pub fn new(typ: tokentypes::ParserTokenType, line: usize, col: usize, file: String) -> Self {
         Self { typ, line, col, file }
     }
+
+    pub fn repr(&self) -> String {
+        use tokentypes::ParserTokenType as TokTyp;
+        match self.typ {
+            TokTyp::Artı => "+".to_string(),
+            TokTyp::ArtıArtı => "++".to_string(),
+            TokTyp::At => "at".to_string(),
+            TokTyp::Bool { val } => {
+                match val {
+                    true => "doğru".to_string(),
+                    false => "yanlış".to_string(),
+                }
+            },
+            TokTyp::Bölü => "/".to_string(),
+            TokTyp::BüyükEşittir => ">=".to_string(),
+            TokTyp::Büyüktür => ">".to_string(),
+            TokTyp::De => "de".to_string(),
+            TokTyp::Değildir => "!".to_string(),
+            TokTyp::Döndür => "dön".to_string(),
+            TokTyp::Eksi => "-".to_string(),
+            TokTyp::EksiEksi => "--".to_string(),
+            TokTyp::EOF => "EOF".to_string(),
+            TokTyp::EşitDeğildir => "!=".to_string(),
+            TokTyp::Eşittir => "=".to_string(),
+            TokTyp::Girdi => "girdi".to_string(),
+            TokTyp::Identifier { ref id } => id.clone(),
+            TokTyp::İken (_) => "iken".to_string(),
+            TokTyp::İkiNoktaNokta => ":.".to_string(),
+            TokTyp::İse(_) => "ise".to_string(),
+            TokTyp::İşlev {..} => "işlev".to_string(),
+            TokTyp::İşlevSonlandır {..} => "son".to_string(),
+            TokTyp::Kopya => "kpy".to_string(),
+            TokTyp::Koy => "->".to_string(),
+            TokTyp::KüçükEşittir => "<=".to_string(),
+            TokTyp::Küçüktür => "<".to_string(),
+            TokTyp::Modulo => "%".to_string(),
+            TokTyp::Sayı { val } => {
+                match val.fract() == 0.0 {
+                    true => format!("{:.0?}", val),
+                    false => format!("{:?}", val),
+                }
+            },
+            TokTyp::Son {..} => "son".to_string(),
+            TokTyp::Takas => "tks".to_string(),
+            TokTyp::Tipinde => "@".to_string(),
+            TokTyp::Ve => "ve".to_string(),
+            TokTyp::Veya => "veya".to_string(),
+            TokTyp::Yazı { ref val } => format!("{:?}", val),
+            TokTyp::Yoksa(_) => "yoksa".to_string(),
+            TokTyp::Çarpı => "*".to_string(),
+            TokTyp::Üst => "üst".to_string(),
+        }
+    }
 }
