fe350b0bb8eb6dc7c363a0aa4069cc74f7443d56
kaiserthe13th <superkerem13@gmail.com>
2021-11-27 13:02:45 +0300

    reading for version 4.0.0

--- src/store.rs
+++ src/store.rs
@@ -3,8 +3,8 @@ use cfg_if::cfg_if;
 pub mod globarg {
     pub static mut SUPRESS_WARN: bool = false;
 }
-pub const VERSION: &str = "0.3.1";
-pub const RELEASE: &str = "16.11.2021";
+pub const VERSION: &str = "0.4.0";
+pub const RELEASE: &str = "27.11.2021";
 cfg_if! {
     if #[cfg(windows)] {
         pub const PATH_SEP: char = '\\';
