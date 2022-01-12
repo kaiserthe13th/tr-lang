4b489a0d00a1f382f2ddf767117ecd87128cd3fa
kaiserthe13th <superkerem13@gmail.com>
2022-01-12 23:36:02 +0300

    rustfmt

--- build.rs
+++ build.rs
@@ -3,5 +3,8 @@ use chrono::Utc;
 fn main() {
     let utc_date = Utc::today();
 
-    println!("cargo:rustc-env=RELEASE_DATE={}", utc_date.format("%d/%m/%Y"));
+    println!(
+        "cargo:rustc-env=RELEASE_DATE={}",
+        utc_date.format("%d/%m/%Y")
+    );
 }
